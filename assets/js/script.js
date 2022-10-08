91871cb11958f82595fbe8a8057a5d72d9d93bb0
jarondlk <jaronchai78@gmail.com>
2022-10-08 17:23:16 +0700

    Add store page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Display Toast Message
+function displayToast() {
+            var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+            var toastList = toastElList.map(function (toastEl) {
+                // Creates an array of toasts (it only initializes them)
+                return new bootstrap.Toast(toastEl) // No need for options; use the default options
+            });
+            toastList.forEach(toast => toast.show()); // This show them
+
+            console.log(toastList); // Testing to see if it works
+        };
